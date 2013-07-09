973e684615e7248eaef4e69fef856ba147614817
Rahul Singh <rahul692000@gmail.com>
2013-07-09 11:13:47 +0530

    activity app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivityHelper::Application.config.secret_token = '9de8e2c2f167c13876a94726135cfb1ec7aa4e725382820a3c01f6c307f7b5bdaddc8d0e3b7496e76444286eac20d0888652cfee30f90ff2fa9ad8867de887a3'
