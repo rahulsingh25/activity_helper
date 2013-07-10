b322d9f0205e763cd2d80f8f64e9a59c9df9a549
Rahul Singh <rahul692000@gmail.com>
2013-07-10 10:27:20 +0530

    generated devise views

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 
+	before { @user=User.new }
+
+	subject { @user }
+
+	it { should respond_to(:email) }
+	it { should respond_to(:encrypted_password) }
+	it { should respond_to(:reset_password_token) }
+
 end
