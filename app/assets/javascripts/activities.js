9112858bd2c17f52107ad2f4bf8df19c7d4c1e28
Rahul Singh <rahul692000@gmail.com>
2013-07-17 12:16:39 +0530

    adding to locals for all users activities

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	$(document).on("ajax:success","#create-activity-form",function(evt,data,status,xhr){
-		//alert(xhr.responseText);
 		$('#activities').find('#activities-partial').replaceWith(xhr.responseText);
 		var el = $('#create-activity-form');
 		el.find('input:text,textarea').val('');
